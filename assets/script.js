5b4fd97639a25c53f6cc2aab16ee1c25e00df040
jasvinder-dev <jasvinder.khera@adaan.com>
2025-03-27 18:37:29 +0530

    Add files via upload
    first commit

---
+++ assets/script.js
@@ -0,0 +1,16 @@
+document.addEventListener("DOMContentLoaded", function () {
+    var swiper = new Swiper(".mySwiper", {
+        slidesPerView: 3, // Show 3 slides at a time
+        spaceBetween: 20, // Add spacing between slides
+        loop: true, // Enables infinite loop
+        autoplay: {
+            delay: 3000, // Auto slide every 3 seconds
+            disableOnInteraction: false,
+        },
+       
+        navigation: {
+            nextEl: ".swiper-button-next",
+            prevEl: ".swiper-button-prev",
+        },
+    });
+});
